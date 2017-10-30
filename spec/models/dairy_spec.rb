a8775d92ca2df77a1edee5d2f65cf68d2002c161
Yosuke Horikawa <uplifting0760@gmail.com>
2017-10-30 19:13:49 +0900

    #48 [add] modelのバリデーションテスト追加

--- spec/models/dairy_spec.rb
+++ spec/models/dairy_spec.rb
@@ -1,5 +1,25 @@
 require "rails_helper"
 
 RSpec.describe Dairy, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validate' do
+    context 'present' do
+      it "Userがあれば、有効であること" do
+        user = User.create(
+          id: 1,
+          email: 'test@test.com',
+          password: 'hogehoge',
+        )
+
+        dairy = user.dairies.create(
+        )
+        expect(dairy).to be_valid
+      end
+
+      it "Userがないと、有効であること" do
+        dairy = Dairy.create(
+        )
+        expect(dairy).not_to be_valid
+      end
+    end
+  end
 end
