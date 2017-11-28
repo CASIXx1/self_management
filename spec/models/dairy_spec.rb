30fdb93d605a512e25a5eb133a74168999d4cba1
Yosuke Horikawa <uplifting0760@gmail.com>
2017-11-28 21:05:48 +0900

    #48 [fix] test修正

--- spec/models/dairy_spec.rb
+++ spec/models/dairy_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
 RSpec.describe Dairy, type: :model do
-  describe 'validate' do
-    context 'present' do
+  describe "validate" do
+    context "present" do
       it "Userがあれば、有効であること" do
         user = User.create(
           id: 1,
-          email: 'test@test.com',
-          password: 'hogehoge',
+          email: "test@test.com",
+          password: "hogehoge",
         )
 
         dairy = user.dairies.create(
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,29 +1,29 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  describe 'validate' do
-    context 'present test' do
-      it 'emailとpasswordがあれば、有効であること' do
+  describe "validate" do
+    context "present test" do
+      it "emailとpasswordがあれば、有効であること" do
         user = User.create(
           id: 1,
-          email: 'test@test.com',
-          password: 'hogehoge',
+          email: "test@test.com",
+          password: "hogehoge",
         )
         expect(user).to be_valid
       end
 
-      it 'emailがない場合、有効でないこと' do
+      it "emailがない場合、有効でないこと" do
         user = User.create(
           id: 1,
-          password: 'hogehoge',
+          password: "hogehoge",
         )
         expect(user).not_to be_valid
       end
 
-      it 'passwordがない場合、有効でないこと' do
+      it "passwordがない場合、有効でないこと" do
         user = User.create(
           id: 1,
-          password: 'hogehoge',
+          password: "hogehoge",
         )
         expect(user).not_to be_valid
       end
